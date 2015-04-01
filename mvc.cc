dd3e3e0702df7a49df8b78b774eea192d5be082c
Wei Ang <weiang123@gmail.com>
2015-04-01 14:33:44 +0800

    Initial commit

---
+++ mvc.cc
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: mvc.cc
+	> Author: Weiang
+	> Mail: weiang@mail.ustc.edu.cn 
+	> Created Time: 2015年04月01日 星期三 14时33分08秒
+    > Describition: 
+ ************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
